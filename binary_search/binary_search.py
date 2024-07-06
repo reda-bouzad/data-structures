e3013e4cedcba591b9e3d0cbd02da891f2860959
reda-bouzad <redabouzadfst@gmail.com>
2024-07-06 10:11:17 +0100

    v2

---
+++ binary_search/binary_search.py
@@ -0,0 +1,23 @@
+# Declarting An array
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# for number in numbers:
+#     print(number)
+
+
+def binary_search(list, item):
+# Declaring Low, high, mid and guess
+    low = 0
+    high = len(list) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None;
+    
+print(binary_search(numbers, 8))
